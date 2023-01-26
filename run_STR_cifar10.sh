60d23df6704fbac83ee28da892d8da44bc700508
WANG Zihao <wangzihao820@hotmail.com>
2023-01-26 20:54:20 +0800

    update evaluation notebook

--- run_STR_cifar10.sh
+++ run_STR_cifar10.sh
@@ -1,7 +1,4 @@
-# for prune_rate in 0.99999 0.999999
-# for prune_rate in 0.0 0.1 0.5 0.8 0.9 0.99 0.999 0.9999
-# for weight_decay in 1e-3 5e-3
-for weight_decay in 1e-3 2e-3 2.5e-3 3e-3 4e-3 5e-3
+for weight_decay in 1e-5 3e-5 1e-4 3e-4 1e-3
 do
     python3 main.py --config configs/largescale/resnet18-str-cifar10.yaml \
                     --weight-decay $weight_decay \
--- run_STR_cifar100.sh
+++ run_STR_cifar100.sh
@@ -1,4 +1,4 @@
-for weight_decay in 1e-3 2e-3 2.5e-3 3e-3 4e-3 5e-3
+for weight_decay in 1e-5 3e-5 1e-4 3e-4 1e-3
 do
     python3 main.py --config configs/largescale/resnet18-str-cifar100.yaml \
                     --weight-decay $weight_decay \
