1e1cb8ac9a7adda79040e478531bd1cee19cae44
WANG Zihao <wangzihao820@hotmail.com>
2023-01-31 14:35:03 +0800

    iterate through the checkpoints

--- eval_checkpoint.py
+++ eval_checkpoint.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import time
 
 from torchvision import datasets, transforms
 
@@ -14,9 +15,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ckpt", type=str)
-parser.add_argument("--thr", type=float, default=1e-3)
+parser.add_argument("--thr", type=str, default="1e-5,1e-4,1e-3,1e-2,1e-1")
 parser.add_argument("--data", type=int, default=10)
 parser.add_argument("--method", type=str, default="VanillaConv")
+parser.add_argument("--output_csv_name", type=str, default="out.csv")
+
 
 class CIFAR10:
     def __init__(self):
@@ -123,13 +126,11 @@ class CIFAR100:
         )
 
 
-cifar10_dataset = CIFAR10()
-cifar100_dataset = CIFAR100()
 
-def evaluate_checkpoint(ckpt_path = "runs/resnet18-l1-cifar10/l1=1e-3/prune_rate=0.0/0/checkpoints/model_best.pth",
-                        dataset = "cifar10",
-                        conv_type = "vanilla",
-                        thr = 1e-3,
+def evaluate_checkpoint(ckpt_path="runs/resnet18-l1-cifar10/l1=1e-3/prune_rate=0.0/0/checkpoints/model_best.pth",
+                        dataset="cifar10",
+                        conv_type="vanilla",
+                        thr=1e-3,
                         device="cuda:0",
                         num_classes=100):
 
@@ -147,8 +148,7 @@ def evaluate_checkpoint(ckpt_path = "runs/resnet18-l1-cifar10/l1=1e-3/prune_rate
             m.thr = thr
             print("setting threshold", n)
 
-    data = cifar10_dataset if dataset == 'cifar10' else cifar100_dataset
-    val_loader = data.val_loader
+    val_loader = dataset.val_loader
 
     top1 = AverageMeter("Acc@1", ":6.2f", write_val=False)
     top5 = AverageMeter("Acc@5", ":6.2f", write_val=False)
@@ -184,7 +184,7 @@ def evaluate_checkpoint(ckpt_path = "runs/resnet18-l1-cifar10/l1=1e-3/prune_rate
     compression_ratio = total_sum / nonzero_sum
 
     # return {"acc": acc, "cr": compression_ratio.item()}
-    print( compression_ratio.item(), acc )
+    return compression_ratio.item(), acc
 
 
 
@@ -198,13 +198,64 @@ def evaluate_checkpoint(ckpt_path = "runs/resnet18-l1-cifar10/l1=1e-3/prune_rate
 # num_classes=100
 # )
 
-args = parser.parse_args()
 
-evaluate_checkpoint(
-    ckpt_path=args.ckpt,
-    dataset=f"cifar{args.data}",
-    conv_type=args.method,
-    thr=args.thr,
-    device="cuda:0",
-    num_classes=args.data
-)
+def identify_ckpt_in_sub_folder(path):
+    print("in", path)
+    if path.endswith("model_best.pth"):
+        ti_m = os.path.getmtime(path)
+        return [(path, time.ctime(ti_m))]
+
+    elif os.path.isdir(path):
+        ret = []
+        for _p in os.listdir(path):
+            p = os.path.join(path, _p)
+            r = identify_ckpt_in_sub_folder(p)
+            if r:
+                ret.extend(r)
+        return ret
+
+    else:
+        return []
+
+
+if __name__ == "__main__":
+
+    
+    args = parser.parse_args()
+    dataset = f"cifar{args.data}"
+    if args.data == 10:
+        dataset = CIFAR10()
+    else:
+        dataset = CIFAR100()
+
+    ckpt_path_time_list = identify_ckpt_in_sub_folder(args.ckpt)
+
+    print(ckpt_path_time_list)
+    
+    from collections import defaultdict
+    data = defaultdict(list)
+
+    
+    for ckpt_path, ckpt_time in ckpt_path_time_list:
+
+        for thr_str in args.thr.split(','):
+            thr = float(thr_str)
+
+            cr, acc = evaluate_checkpoint(
+                ckpt_path=ckpt_path,
+                dataset=dataset,
+                conv_type=args.method,
+                thr=thr,
+                device="cuda:0",
+                num_classes=args.data
+            )
+
+            data['cr'].append(cr)
+            data['acc'].append(acc)
+            data['ckpt_path'] = ckpt_path
+            data['ckpt_time'] = ckpt_time
+
+    import pandas as pd
+    df = pd.DataFrame(data)
+    df.to_csv(args.output_csv_name, index=False)
+    print(df.to_string())
